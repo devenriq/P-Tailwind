a94676ea2d136848425c407be2d15edd2de59420
Enrique <octepc.97@gmail.com>
2022-08-09 13:40:55 -0500

    configured the project

---
+++ postcss.config.js
@@ -0,0 +1,10 @@
+// module.exports = {
+//   plugin: [require("tailwindcss"), require("autoprefixer")],
+// };
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+};
