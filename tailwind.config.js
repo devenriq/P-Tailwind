51cf881f4489ba2cf31501a452f53bc0ffaaa5bb
Enrique <octepc.97@gmail.com>
2022-08-09 20:27:36 -0500

    learned pseudoclass and other variants

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit",
-  content: ["./src/index.html", "./src/display.html", "./src/flex.html"],
-
+  // content: ["./src/index.html", "./src/display.html", "./src/flex.html"],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+  },
+  variants: {
+    opacity: ["responsive", "hover", "focus", "disabled"],
   },
   plugins: [],
 };
